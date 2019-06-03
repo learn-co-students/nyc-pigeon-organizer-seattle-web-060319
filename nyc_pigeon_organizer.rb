a40e80b6a5e2c24282c1d48a1056f086a69af348
Justin <sumbhodi1618@gmail.com>
2019-06-03 05:28:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+       array.each do |name|
+         binding.pry
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+       end
+    end
+  end
+ new_hash
 end
