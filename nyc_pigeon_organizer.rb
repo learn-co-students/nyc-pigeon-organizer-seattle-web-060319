2aa7b9a1338252b09500e25cd757e7058c68e467
Kara Odle <karajo.odle@gmail.coml>
2019-06-17 21:26:48 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,57 +17,93 @@ pigeon_data = {
   }
 }
 
+# def nyc_pigeon_organizer(data)
+#   new_hash = {}
+#   data[:gender][:male].each do |male_name|
+#     new_hash[male_name] = {}
+#     new_hash[male_name][:gender] = []
+#     new_hash[male_name][:lives] = []
+#     new_hash[male_name][:color] = []
+#
+#     new_hash[male_name][:gender] << "male"
+#
+#
+#     data[:color].each do |color, array|
+#
+#       if array.include?(male_name)
+#         new_hash[male_name][:color] << color.to_s
+#       end
+#     end
+#
+#     data[:lives].each do |location, array|
+#
+#       if array.include?(male_name)
+#         new_hash[male_name][:lives] << location
+#       end
+#     end
+#
+#   end
+#
+#   data[:gender][:female].each do |female_name|
+#     new_hash[female_name] = {}
+#     new_hash[female_name][:gender] = []
+#     new_hash[female_name][:lives] = []
+#     new_hash[female_name][:color] = []
+#
+#     new_hash[female_name][:gender] << "female"
+#
+#
+#     data[:color].each do |color, array|
+#
+#       if array.include?(female_name)
+#         new_hash[female_name][:color] << color.to_s
+#       end
+#     end
+#
+#     data[:lives].each do |location, array|
+#
+#       if array.include?(female_name)
+#         new_hash[female_name][:lives] << location
+#       end
+#     end
+#
+#   end
+#
+# new_hash
+# end
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data[:gender][:male].each do |male_name|
-    new_hash[male_name] = {}
-    new_hash[male_name][:gender] = []
-    new_hash[male_name][:lives] = []
-    new_hash[male_name][:color] = []
+new_hash = {}
+names = data[:gender][:male] + data[:gender][:female]
+names.each do |name|
+  new_hash[name] = {}
+  new_hash[name][:gender] = []
+  new_hash[name][:lives] = []
+  new_hash[name][:color] = []
 
-    new_hash[male_name][:gender] << "male"
 
 
-    data[:color].each do |color, array|
 
-      if array.include?(male_name)
-        new_hash[male_name][:color] << color.to_s
-      end
+     data[:color].each do |color, array|
+
+       if array.include?(name)
+         new_hash[name][:color] << color.to_s
+       end
+     end
+
+     data[:lives].each do |location, array|
+
+       if array.include?(name)
+         new_hash[name][:lives] << location
+       end
+     end
+
+     data[:gender].each do |gender, array|
+
+       if array.include?(name)
+         new_hash[name][:gender] << gender.to_s
+       end
     end
 
-    data[:lives].each do |location, array|
-
-      if array.include?(male_name)
-        new_hash[male_name][:lives] << location
-      end
-    end
-
+   end
+  new_hash
   end
-
-  data[:gender][:female].each do |female_name|
-    new_hash[female_name] = {}
-    new_hash[female_name][:gender] = []
-    new_hash[female_name][:lives] = []
-    new_hash[female_name][:color] = []
-
-    new_hash[female_name][:gender] << "female"
-
-
-    data[:color].each do |color, array|
-
-      if array.include?(female_name)
-        new_hash[female_name][:color] << color.to_s
-      end
-    end
-
-    data[:lives].each do |location, array|
-
-      if array.include?(female_name)
-        new_hash[female_name][:lives] << location
-      end
-    end
-
-  end
-
-new_hash
-end
